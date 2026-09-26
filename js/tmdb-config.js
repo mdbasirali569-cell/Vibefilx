c91c260c38d9af25537117ce4861baa170cb6f6b
mdbasirali569-cell <mdbasirali569@gmail.com>
2026-09-26 09:37:53 +0600

    Add TMDB API configuration placeholder

---
+++ js/tmdb-config.js
@@ -0,0 +1,7 @@
+/* TMDB configuration
+   Get your TMDB API Read Access Token from your TMDB account API settings.
+   Keep this file out of public repos for production. For a static demo, paste the token here only if you understand it will be visible to site visitors.
+*/
+window.TMDB_CONFIG = {
+    accessToken: ""
+};
